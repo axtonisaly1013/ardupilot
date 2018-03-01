c4f6346c28aaecb483027f8f306f7cda975d5ab4
axtonisaly1013 <axtonisaly1013@ufl.edu>
2018-03-01 10:30:41 -0500

    Turned logging velocity back on

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.8.5beta1"
+#define THISFIRMWARE "STUXNET V0.1.0alpha1"
 #define FIRMWARE_VERSION 3,8,5,FIRMWARE_VERSION_TYPE_BETA
 
 #ifndef GIT_VERSION

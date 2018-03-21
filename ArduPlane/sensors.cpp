6c8b729416859462431d06eeb6d7546f999924b4
axtonisaly1013 <axtonisaly1013@ufl.edu>
2018-03-21 14:29:45 -0400

    Switched to AP_Float datatype

--- ArduPlane/sensors.cpp
+++ ArduPlane/sensors.cpp
@@ -58,9 +58,9 @@ void Plane::read_rangefinder(void)
 		rngdt = current_time_rng-prev_rng_time;
 	}
 	prev_rng_time = current_time_rng;
-	float prev_dist = dist_above_water;
-	float DAW[rangefinder.num_sensors()];
-	float VAW[rangefinder.num_sensors()];
+	AP_Float prev_dist = dist_above_water;
+	AP_Float DAW[rangefinder.num_sensors()];
+	AP_Float VAW[rangefinder.num_sensors()];
 	float d_sum = 0;
 	float v_sum = 0;
 	uint8_t compensation_mode = rangefinder.get_att_Comp();
